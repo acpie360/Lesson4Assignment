8fa55e9f373961a3c9d68477d74b79cbd110b9ea
Alan Chen <alan.chen@hunterindustries.com>
2020-07-25 14:35:00 -0700

    Add project files.

---
+++ HamburgerStand.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Lesson4Assignment
+{
+    partial class HamburgerStand
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpOrderMenu = new System.Windows.Forms.GroupBox();
+            this.chkHamburger = new System.Windows.Forms.CheckBox();
+            this.chkFries = new System.Windows.Forms.CheckBox();
+            this.chkDrink = new System.Windows.Forms.CheckBox();
+            this.lblHamburgerPrice = new System.Windows.Forms.Label();
+            this.lblFriesPrice = new System.Windows.Forms.Label();
+            this.lblDrinkPrice = new System.Windows.Forms.Label();
+            this.grpDesert = new System.Windows.Forms.GroupBox();
+            this.radCake = new System.Windows.Forms.RadioButton();
+            this.radIceCream = new System.Windows.Forms.RadioButton();
+            this.btnOrder = new System.Windows.Forms.Button();
+            this.lblReceipt = new System.Windows.Forms.Label();
+            this.grpOrderMenu.SuspendLayout();
+            this.grpDesert.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpOrderMenu
+            // 
+            this.grpOrderMenu.Controls.Add(this.lblDrinkPrice);
+            this.grpOrderMenu.Controls.Add(this.lblFriesPrice);
+            this.grpOrderMenu.Controls.Add(this.lblHamburgerPrice);
+            this.grpOrderMenu.Controls.Add(this.chkDrink);
+            this.grpOrderMenu.Controls.Add(this.chkFries);
+            this.grpOrderMenu.Controls.Add(this.chkHamburger);
+            this.grpOrderMenu.Location = new System.Drawing.Point(34, 37);
+            this.grpOrderMenu.Name = "grpOrderMenu";
+            this.grpOrderMenu.Size = new System.Drawing.Size(722, 170);
+            this.grpOrderMenu.TabIndex = 0;
+            this.grpOrderMenu.TabStop = false;
+            this.grpOrderMenu.Text = "Order Menu";
+            // 
+            // chkHamburger
+            // 
+            this.chkHamburger.AutoSize = true;
+            this.chkHamburger.Location = new System.Drawing.Point(20, 35);
+            this.chkHamburger.Name = "chkHamburger";
+            this.chkHamburger.Size = new System.Drawing.Size(101, 21);
+            this.chkHamburger.TabIndex = 0;
+            this.chkHamburger.Text = "Hamburger";
+            this.chkHamburger.UseVisualStyleBackColor = true;
+            // 
+            // chkFries
+            // 
+            this.chkFries.AutoSize = true;
+            this.chkFries.Location = new System.Drawing.Point(20, 78);
+            this.chkFries.Name = "chkFries";
+            this.chkFries.Size = new System.Drawing.Size(61, 21);
+            this.chkFries.TabIndex = 1;
+            this.chkFries.Text = "Fries";
+            this.chkFries.UseVisualStyleBackColor = true;
+            // 
+            // chkDrink
+            // 
+            this.chkDrink.AutoSize = true;
+            this.chkDrink.Location = new System.Drawing.Point(20, 121);
+            this.chkDrink.Name = "chkDrink";
+            this.chkDrink.Size = new System.Drawing.Size(63, 21);
+            this.chkDrink.TabIndex = 2;
+            this.chkDrink.Text = "Drink";
+            this.chkDrink.UseVisualStyleBackColor = true;
+            // 
+            // lblHamburgerPrice
+            // 
+            this.lblHamburgerPrice.Location = new System.Drawing.Point(355, 35);
+            this.lblHamburgerPrice.Name = "lblHamburgerPrice";
+            this.lblHamburgerPrice.Size = new System.Drawing.Size(332, 23);
+            this.lblHamburgerPrice.TabIndex = 3;
+            this.lblHamburgerPrice.Text = ".....";
+            this.lblHamburgerPrice.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblFriesPrice
+            // 
+            this.lblFriesPrice.Location = new System.Drawing.Point(355, 78);
+            this.lblFriesPrice.Name = "lblFriesPrice";
+            this.lblFriesPrice.Size = new System.Drawing.Size(332, 23);
+            this.lblFriesPrice.TabIndex = 4;
+            this.lblFriesPrice.Text = ".....";
+            this.lblFriesPrice.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblDrinkPrice
+            // 
+            this.lblDrinkPrice.Location = new System.Drawing.Point(355, 121);
+            this.lblDrinkPrice.Name = "lblDrinkPrice";
+            this.lblDrinkPrice.Size = new System.Drawing.Size(332, 23);
+            this.lblDrinkPrice.TabIndex = 5;
+            this.lblDrinkPrice.Text = ".....";
+            this.lblDrinkPrice.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // grpDesert
+            // 
+            this.grpDesert.Controls.Add(this.radIceCream);
+            this.grpDesert.Controls.Add(this.radCake);
+            this.grpDesert.Location = new System.Drawing.Point(34, 233);
+            this.grpDesert.Name = "grpDesert";
+            this.grpDesert.Size = new System.Drawing.Size(722, 102);
+            this.grpDesert.TabIndex = 1;
+            this.grpDesert.TabStop = false;
+            this.grpDesert.Text = "Free Desert";
+            // 
+            // radCake
+            // 
+            this.radCake.AutoSize = true;
+            this.radCake.Location = new System.Drawing.Point(20, 40);
+            this.radCake.Name = "radCake";
+            this.radCake.Size = new System.Drawing.Size(61, 21);
+            this.radCake.TabIndex = 0;
+            this.radCake.TabStop = true;
+            this.radCake.Text = "Cake";
+            this.radCake.UseVisualStyleBackColor = true;
+            // 
+            // radIceCream
+            // 
+            this.radIceCream.AutoSize = true;
+            this.radIceCream.Location = new System.Drawing.Point(361, 40);
+            this.radIceCream.Name = "radIceCream";
+            this.radIceCream.Size = new System.Drawing.Size(92, 21);
+            this.radIceCream.TabIndex = 1;
+            this.radIceCream.TabStop = true;
+            this.radIceCream.Text = "Ice Cream";
+            this.radIceCream.UseVisualStyleBackColor = true;
+            // 
+            // btnOrder
+            // 
+            this.btnOrder.Location = new System.Drawing.Point(622, 366);
+            this.btnOrder.Name = "btnOrder";
+            this.btnOrder.Size = new System.Drawing.Size(134, 48);
+            this.btnOrder.TabIndex = 2;
+            this.btnOrder.Text = "ORDER";
+            this.btnOrder.UseVisualStyleBackColor = true;
+            this.btnOrder.Click += new System.EventHandler(this.btnOrder_Click);
+            // 
+            // lblReceipt
+            // 
+            this.lblReceipt.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblReceipt.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblReceipt.Location = new System.Drawing.Point(34, 442);
+            this.lblReceipt.Name = "lblReceipt";
+            this.lblReceipt.Size = new System.Drawing.Size(485, 236);
+            this.lblReceipt.TabIndex = 3;
+            this.lblReceipt.Text = "Receipt";
+            // 
+            // HamburgerStand
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 706);
+            this.Controls.Add(this.lblReceipt);
+            this.Controls.Add(this.btnOrder);
+            this.Controls.Add(this.grpDesert);
+            this.Controls.Add(this.grpOrderMenu);
+            this.Name = "HamburgerStand";
+            this.Text = "Hamburger Stand";
+            this.grpOrderMenu.ResumeLayout(false);
+            this.grpOrderMenu.PerformLayout();
+            this.grpDesert.ResumeLayout(false);
+            this.grpDesert.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpOrderMenu;
+        private System.Windows.Forms.Label lblDrinkPrice;
+        private System.Windows.Forms.Label lblFriesPrice;
+        private System.Windows.Forms.Label lblHamburgerPrice;
+        private System.Windows.Forms.CheckBox chkDrink;
+        private System.Windows.Forms.CheckBox chkFries;
+        private System.Windows.Forms.CheckBox chkHamburger;
+        private System.Windows.Forms.GroupBox grpDesert;
+        private System.Windows.Forms.RadioButton radIceCream;
+        private System.Windows.Forms.RadioButton radCake;
+        private System.Windows.Forms.Button btnOrder;
+        private System.Windows.Forms.Label lblReceipt;
+    }
+}
+
---
+++ HamburgerStand.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lesson4Assignment
+{
+    public partial class HamburgerStand : Form
+    {
+        private const int maxItems = 3;
+        private const double priceHamburger = 2.0;
+        private const double priceFries = 1.5;
+        private const double priceDrink = 1.0;
+        private const double priceDesert = 0;
+
+        public enum MENU_ITEM {NONE, HAMBURGER, FRIES, DRINK};
+        public enum DESERT_ITEM {NONE, CAKE, ICE_CREAM};
+
+        class Order
+        {
+            public MENU_ITEM[] menu_item;
+            public DESERT_ITEM desert;
+            public double price;
+
+            private readonly string[] food_name = 
+            {
+                "Hamburger",
+                "Fries    ",
+                "Drink    ",
+                "Cake     ",
+                "Ice Cream"
+            };
+
+            public Order()
+            {
+                menu_item = new MENU_ITEM[maxItems];
+
+                for (int i = 0; i < menu_item.Length; i++)
+                {
+                    menu_item[i] = MENU_ITEM.NONE;
+                }
+
+                desert = DESERT_ITEM.NONE;
+                price = 0;
+            }
+
+            public string ShowOrder()
+            {
+                int i;
+                string order_msg = String.Format("{0,25}", "Your Order\n");
+
+                for (i = 0; i < menu_item.Length; i++)
+                {
+                    switch (menu_item[i])
+                    {
+                        case MENU_ITEM.NONE:
+                            continue;
+
+                        case MENU_ITEM.HAMBURGER:
+                            order_msg += String.Format("{0,-12}{1,25:C}\n", food_name[0], priceHamburger);
+                            break;
+
+                        case MENU_ITEM.FRIES:
+                            order_msg += String.Format("{0,-12}{1,25:C}\n", food_name[1], priceFries);
+                            break;
+
+                        case MENU_ITEM.DRINK:
+                            order_msg += String.Format("{0,-12}{1,25:C}\n", food_name[2], priceDrink);// + Environment.NewLine;
+                            break;
+                    }
+                }
+
+                switch (desert)
+                {
+                    case DESERT_ITEM.NONE:
+                        break;
+
+                    case DESERT_ITEM.CAKE:
+                        order_msg += String.Format("{0,-12}{1,25:C}\n", food_name[3], priceDesert);
+                        break;
+                    case DESERT_ITEM.ICE_CREAM:
+                        order_msg += String.Format("{0,-12}{1,25:C}\n", food_name[4], priceDesert);
+                        break;
+                }
+
+                order_msg += String.Format("\n\n{0,20}{1,16:C}\n", "Total", price);// + Environment.NewLine;
+
+                return order_msg;
+            }
+        }
+
+        public HamburgerStand()
+        {
+            InitializeComponent();
+
+            lblHamburgerPrice.Text = String.Format("{0, 10:C}", priceHamburger);
+            lblFriesPrice.Text = String.Format("{0,10:C}", priceFries);
+            lblDrinkPrice.Text = String.Format("{0,10:C}", priceDrink);
+        }
+
+        private void btnOrder_Click(object sender, EventArgs e)
+        {
+            Order myOrder = new Order();
+
+            if (chkHamburger.Checked == false && chkFries.Checked == false && chkDrink.Checked == false)
+            {
+                MessageBox.Show("Please select one item from the menu", "Missing Item");
+                return;
+            }
+
+            if (chkHamburger.Checked == true)
+            {
+                myOrder.menu_item[0] = MENU_ITEM.HAMBURGER;
+                myOrder.price += priceHamburger;
+            }
+
+            if (chkFries.Checked == true)
+            {
+                myOrder.menu_item[1] = MENU_ITEM.FRIES;
+                myOrder.price += priceFries;
+            }
+
+            if (chkDrink.Checked == true)
+            {
+                myOrder.menu_item[2] = MENU_ITEM.DRINK;
+                myOrder.price += priceDrink;
+            }
+
+            if (radCake.Checked == true)
+            {
+                myOrder.desert = DESERT_ITEM.CAKE;
+            }
+
+            if (radIceCream.Checked == true)
+            {
+                myOrder.desert = DESERT_ITEM.ICE_CREAM;
+            }
+
+            lblReceipt.Text = myOrder.ShowOrder();
+        }
+    }
+}
